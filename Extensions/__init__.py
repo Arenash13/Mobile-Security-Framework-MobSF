8dad44f45d332aff2fef663a7276ca9ab720e41a
Arenash13 <remaxm25@gmail.com>
2020-03-10 15:12:32 +0100

    add template load

--- Extensions/__init__.py
+++ Extensions/__init__.py
@@ -10,15 +10,14 @@ from abc import ABC
 
 logger = logging.getLogger(__name__)
 
-'''
+def static_analysis_extension(app_path, platform, typ, code_an_dic):
+    """
     Will search for every class that extends the class StaticAnalysisExtension in this module and 
     start the assesments that are defined in it.
-'''
-def static_analysis_extension(app_path, platform, typ, code_an_dic):
+    """
     module_platform = importlib.import_module(
         "Extensions.static_analysis.{}".format(platform))
 
-    custom_analysis = {}
     for(_, name, _) in pkgutil.iter_modules(module_platform.__path__):
         module = importlib.import_module(
             "{}.{}".format(android.__name__, name))
@@ -28,3 +27,4 @@ def static_analysis_extension(app_path, platform, typ, code_an_dic):
                 analysis = analysis_class()
                 logger.info("Starting additional static analysis defined in {}".format(module.__name__))
                 analysis.perform_analysis(app_path, typ, code_an_dic)
+                logger.info(analysis.generate_template())
--- MobSF/static_analysis_extension.py
+++ MobSF/static_analysis_extension.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractclassmethod
 class StaticAnalysisExtension(ABC):
-    '''
+    """
     This interface allows you to run custom analysis scripts that will be added to MobSF report
-    '''
+    """
 
     @abstractclassmethod
     def perform_analysis(self, app_dir, typ, code_an_dic):
